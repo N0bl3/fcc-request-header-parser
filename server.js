ff89994e334085585de7ea3fd056e124105b302c
Alex N <carbonealpha@gmail.com>
2016-02-22 17:17:35 +0000

    Added a parser for OS detection

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 var express = require('express');
 
 var app = express();
-
-app.get("/", function(){
-  app.redirect("/me");
+var useragent = require('express-useragent');
+app.use(useragent.express());
+app.enable("trust proxy");
+app.get("/", function(req, res){
+  res.redirect("/me");
 });
 
 app.get("/me", function(req, res){
   var me = {
     ip: req.ip,
-    software: req.get("User-Agent"),
+    software: req.useragent.os,
     language: req.get("Accept-Language")
   };
   res.end(JSON.stringify(me));
